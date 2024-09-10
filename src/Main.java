696c2961b1f4dac5c111abecb191ce1fce2b9bea
Milosz463 <miloszdawiec6@gmail.com>
2024-09-10 21:53:10 +0200

    aktualizacja

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
@@ -6,6 +6,22 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         System.out.println('sigma');
+
+         int liczba= (int)(Math.random()*100);
+        System.out.println(liczba);
+
+        Scanner klawiatura = new Scanner(System.in);
+
+        System.out.println("Podaj twoj numer: ");
+        int sigma=klawiatura.nextInt();
+
+        if(sigma==liczba){
+            System.out.println("Twoj numer w kolejce to: "+ liczba);
+        }else{
+            System.out.println("Zapisz sie do kolejki");
+        }
+
+
 
     }
 }
